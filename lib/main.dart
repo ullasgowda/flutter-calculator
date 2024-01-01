ae3233198b48a576e593b288d30b942e459f58f0
Ullas <ullasgowda42@gmail.com>
2024-01-01 20:36:30 +0530

    feat: implement calculator logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,92 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Calculator());
+  runApp(const MyCalculator());
 }
 
-class Calculator extends StatelessWidget {
+class MyCalculator extends StatelessWidget {
+  const MyCalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My Calculator",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Calculator(),
+    );
+  }
+}
+
+class Calculator extends StatefulWidget {
   const Calculator({super.key});
 
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  String output = "";
+  String _output = "";
+
+  double num1 = 0.0;
+  double num2 = 0.0;
+
+  String operand = "";
+
+  Map<String, IconData> iconMap = {
+    "+": Icons.add,
+    "-": Icons.remove,
+    "x": Icons.close,
+    "/": Icons.percent
+  };
+
   buttonPressed(String buttonText) {
-    if (buttonText == 'Clear') {}
-    return true;
+    if (buttonText == 'CLEAR') {
+      _output = "";
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "/" ||
+        buttonText == "X") {
+      num1 = double.parse(output);
+      operand = buttonText;
+      _output = "";
+    } else if (buttonText == ".") {
+      if (_output.contains(".")) {
+        print("Already contains a decimals");
+        return;
+      } else {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "=") {
+      num2 = double.parse(output);
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      }
+      if (operand == "-") {
+        _output = (num1 - num2).toString();
+      }
+      if (operand == "X") {
+        _output = (num1 * num2).toString();
+      }
+      if (operand == "/") {
+        _output = (num1 / num2).toString();
+      }
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+
+    setState(() {
+      output = _output.toString().trim();
+      operand = operand;
+    });
   }
 
   Widget buildButton({required String label}) {
@@ -42,9 +119,9 @@ class Calculator extends StatelessWidget {
           title: const Center(
             child: Text("Calculator"),
           ),
-          backgroundColor: Colors.teal,
+          backgroundColor: Colors.teal[700],
         ),
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.teal[700],
         body: SafeArea(
           child: Container(
             padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -52,21 +129,31 @@ class Calculator extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        "result",
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 20,
+                      alignment: Alignment.bottomLeft,
+                      child: Icon(
+                        iconMap[operand],
+                        color: Colors.teal[900],
+                      ),
+                    ),
+                    Container(
+                      width: 252,
+                      alignment: Alignment.bottomRight,
+                      margin: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Text(
+                        output,
                         style: const TextStyle(
                           color: Colors.white,
                           fontSize: 40,
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
                 const SizedBox(
                   height: 10,
